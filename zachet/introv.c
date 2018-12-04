772282c1a0acce004a059362cf0caab74ead1e01
Kirill Anokhin <anokhin.ka@phystach.edu>
2018-12-05 02:50:57 +0300

    fix

--- zachet/introv.c
+++ zachet/introv.c
@@ -9,40 +9,61 @@
 
 #define BUF_SIZE 128
 
+
+#define START_SOP()					\
+	({						\
+		int tmp = semop(semid, sops, n_sops);	\
+		n_sops = 0;				\
+		tmp;					\
+	})
+
+#define ADD_SOP(num, op, flg)			\
+	do {					\
+		sops[n_sops].sem_num = num;	\
+		sops[n_sops].sem_op  = op;	\
+		sops[n_sops].sem_flg = flg;	\
+		n_sops++;			\
+	} while (0)
+
 int main()
 {
 	char str[BUF_SIZE];
-	strcpy(str, "Hello, world\n");
 	key_t key;
-	char pathname[] = "introv.c";
-	key = ftok(pathname, 0);
 	int semid;
+	char pathname[] = "introv.c";
+	struct sembuf sops[1];
+	size_t n_sops = 0;
+
+	key = ftok(pathname, 0);
 	semid = semget(key, 1, 0666 | IPC_CREAT);
 
-	struct sembuf sops[2];
-	
-	sops[0].sem_num = 0;
-	sops[0].sem_op = 0;
-	sops[0].sem_flg = IPC_NOWAIT;
-	
-	sops[1].sem_num = 0;
-	sops[1].sem_op = 1;
-	sops[1].sem_flg = SEM_UNDO;
-	
-	if (semop(semid, sops, 2) == -1) {
-		strcmp(str, "Goodbye, world\n");
+	ADD_SOP(0, 1, SEM_UNDO);
+	if (START_SOP() == -1) {
+		perror("semop");
+		return 0;
 	}
-	int i = 0;		
-	while (str[i] != '\0') {
-		write(STDOUT_FILENO, str + i, 1);
-		i++;
+
+	ADD_SOP(0, -2, IPC_NOWAIT);
+	ADD_SOP(0,  2, 0);
+	if (START_SOP() == -1) {
+		if (errno != EAGAIN) {
+			perror("semop");
+			return 0;
+		}
+		strcpy(str, "Hello, world\n");
+	} else {
+		strcpy(str, "Goodbye, world\n");
+	}
+
+	for(char* p = str; *p != '\0'; p++) {	
+		write(STDOUT_FILENO, p, 1);
 		sleep(1);
 	}
-	
-	sops[0].sem_num = 0;
-	sops[0].sem_op = -1;
-	sops[0].sem_flg = SEM_UNDO;
-	
-	semop(semid, sops, 1);
+
+	ADD_SOP(0, 1, SEM_UNDO);
+	if (START_SOP() == -1) {
+		perror("semop");
+		return 0;
+	}
 	return 0;
 }
